8ffe1ddca28bd60d43144744d56982e994cae50a
Aswathi P <95677233+Aswathip2023@users.noreply.github.com>
2023-02-28 11:18:31 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,38 +1,35 @@
-from . import views
-from django.urls import path
-from django.contrib.auth import views as auth_views
+"""WeddingManagement URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.urls import path, include
+
+# admin.site.unregister(Group)
 
 urlpatterns = [
-    path('', views.demo, name='demo'),
-    # path('', views.activateEmail, name='email'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('user', views.user, name='user'),
-    path('logout', views.logout, name='logout'),
-    path('cart/logout/', views.logout, name='logout'),
-    path('ShoppingApp/shopping/shopping/logout/', views.logout, name='logout'),
-    path('appointment', views.Bookings, name='appointment'),
-    path('appointment_info', views.appointment_info, name='appointment_info'),
-    path('accounts/login/login',views.login, name='login'),
-    path('accounts/login/register',views.register, name='register'),
-    path('Bridal', views.bridal, name='bridal'),
-
-
-
-    path('gallery', views.gallery, name='gallery'),
-
-    # CRUD OPERATIONS
-    path('delete/<str:id>', views.Delete, name='delete'),
-    path('<str:id>', views.Update, name='update'),
-
-    ####reset password urls####
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('accounts/login/',views.login, name='login'),
-
-
-
+    path('admin/', admin.site.urls),
+    path('', include('WeddingApp.urls')),
+    path('ShoppingApp/', include('ShoppingApp.urls')),
+    path('search/', include('Search_App.urls')),
+    path('cart/', include('cart.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
