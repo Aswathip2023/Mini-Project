6e78ef314743280ae4f18db170b1e432c23af5fe
Aswathi P <95677233+Aswathip2023@users.noreply.github.com>
2023-02-28 10:25:10 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WeddingAppConfig(AppConfig):
+class CartAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Wedding_App'
+    name = 'cart'
