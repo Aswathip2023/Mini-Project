fca8a5604dd131bd7d28fc912745dbafa92d75e8
Aswathi P <95677233+Aswathip2023@users.noreply.github.com>
2023-05-16 10:15:47 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
-"""WeddingManagement URL Configuration
+"""wedding URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,17 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.urls import path, include
 
-# admin.site.unregister(Group)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('WeddingApp.urls')),
-    path('ShoppingApp/', include('ShoppingApp.urls')),
-    path('search/', include('Search_App.urls')),
-    path('cart/', include('cart.urls')),
+    path('',include('wedding_ap.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
